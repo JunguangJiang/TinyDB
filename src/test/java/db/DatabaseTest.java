f4b38bbcf6cd1b2775de3c9f2d7b2a4706ab7dc0
JunguangJiang <13126830206@163.com>
2019-06-07 22:02:49 +0800

    add Clock algorithm in BufferPool

--- src/test/java/db/DatabaseTest.java
+++ src/test/java/db/DatabaseTest.java
@@ -1,5 +1,6 @@
 package db;
 
+import db.error.SQLError;
 import db.file.Table;
 import db.query.QueryResult;
 import org.junit.Test;
@@ -11,35 +12,43 @@ public class DatabaseTest {
     public void createTable() {
         GlobalManager.getCatalog().createDatabase("database");
         GlobalManager.getCatalog().useDatabase("database");
+        try {
+            QueryResult queryResult = GlobalManager.getDatabase().createTable("table", Utility.getTupleDesc(3, "tup"));
+            assertTrue(queryResult.succeeded());
+            Table table = GlobalManager.getDatabase().getTable("table");
+            assertNotNull(table);
+            assertEquals(table.getName(), "table");
 
-        QueryResult queryResult = GlobalManager.getDatabase().createTable("table", Utility.getTupleDesc(3, "tup"));
-        assertTrue(queryResult.succeeded());
-        Table table = GlobalManager.getDatabase().getTable("table");
-        assertNotNull(table);
-        assertEquals(table.getName(), "table");
+            // 2 table cannot have the same name
+            queryResult = GlobalManager.getDatabase().createTable("table", Utility.getTupleDesc(3, "tup"));
+            assertFalse(queryResult.succeeded());
 
-        // 2 table cannot have the same name
-        queryResult = GlobalManager.getDatabase().createTable("table", Utility.getTupleDesc(3, "tup"));
-        assertFalse(queryResult.succeeded());
-
-        GlobalManager.getCatalog().dropDatabase("database");
+            GlobalManager.getCatalog().dropDatabase("database");
+        } catch (SQLError e){
+            fail();
+        }
 
     }
 
     @Test
     public void getTableId() {
-        GlobalManager.getCatalog().createDatabase("database");
-        GlobalManager.getCatalog().useDatabase("database");
+        try {
+            GlobalManager.getCatalog().createDatabase("database");
+            GlobalManager.getCatalog().useDatabase("database");
 
-        QueryResult queryResult = GlobalManager.getDatabase().createTable("table1", Utility.getTupleDesc(3, "tup"));
-        assertTrue(queryResult.succeeded());
-        int id1 = GlobalManager.getDatabase().getTableId("table1");
+            QueryResult queryResult = GlobalManager.getDatabase().createTable("table1", Utility.getTupleDesc(3, "tup"));
+            assertTrue(queryResult.succeeded());
+            int id1 = GlobalManager.getDatabase().getTableId("table1");
 
-        queryResult = GlobalManager.getDatabase().createTable("table2", Utility.getTupleDesc(3, "tup"));
-        assertTrue(queryResult.succeeded());
-        int id2 = GlobalManager.getDatabase().getTableId("table2");
-        assertEquals(id1 + 1, id2);
+            queryResult = GlobalManager.getDatabase().createTable("table2", Utility.getTupleDesc(3, "tup"));
+            assertTrue(queryResult.succeeded());
+            int id2 = GlobalManager.getDatabase().getTableId("table2");
+            assertEquals(id1 + 1, id2);
 
-        GlobalManager.getCatalog().dropDatabase("database");
+            GlobalManager.getCatalog().dropDatabase("database");
+        } catch (SQLError e){
+            fail();
+        }
+
     }
 }
